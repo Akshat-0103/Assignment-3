20fe9b4d9fd2389ac981eb9cc3acbfd59cd0cdd6
Akshat-0103 <akshatshri0103@gmail.com>
2025-08-13 21:58:02 +0530

    Task1.py

--- Task1.py
+++ Task1.py
@@ -1,12 +1,9 @@
-# Function to calculate factorial
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
 
-# Sample number
 number = 5
 
-# Call the function and display the result
 print("The factorial of", number, "is:", factorial(number))
